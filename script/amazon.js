12264c9f22364e7cc3bd06bb0fda458f8c747de5
MD.Siam Kazi <itzsksiam@gmail.com>
2025-09-24 22:59:11 +0600

    Added Product data to The Homepage

--- script/amazon.js
+++ script/amazon.js
@@ -1,18 +1,41 @@
-function shuffle(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    // pick random index from 0 to i
-    const j = Math.floor(Math.random() * (i + 1));
-    
-    // swap array[i] with array[j]
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-}
+import {products} from "../data/products.js";
 
-// Example usage:
-let mobiles = ["iPhone", "Samsung", "Xiaomi", "OnePlus", "Pixel", "Realme"];
 
-console.log("Before shuffle:", mobiles);
+//Access Cart Quantity
 
-let shuffled = shuffle([...mobiles]); // use spread to not change original
-console.log("After shuffle:", shuffled);
+let cartQuantity = document.querySelector(".cart-quantity");
+let productArea = document.querySelector(".products-grid");
+
+let productCard = "";
+products.forEach((product) => {
+   productCard += `<div class="product-card">
+            <div class="product-img">
+                <img src="${product.image}">
+            </div>
+            <div class="product-name">
+                ${product.name}
+            </div>
+            <div class="product-rating">
+                <img src="images/ratings/rating-${product.rating.stars * 10}.png">
+                <span class="rating-quantity">${product.rating.count}</span>
+            </div>
+            <div class="product-price">
+                $${product.priceCents}
+            </div>
+            <div class="order-quantity">
+                <select class="product-order-select">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </select>
+            </div>
+            <div class="product-spacing"></div>
+            <div class="added-to-cart">Added To Cart</div>
+
+            
+            <button class="add-cart-btn">Add to Cart</button>
+        </div>`;
+})
+productArea.innerHTML = productCard;
