ec1cc186c0d718abfbba6e0d957ee6d0d060fe38
MD.Siam Kazi <itzsksiam@gmail.com>
2025-09-25 19:56:02 +0600

    Add random product feature

--- script/amazon.js
+++ script/amazon.js
@@ -1,10 +1,19 @@
-import {products} from "../data/products.js";
+import { cart } from "../data/cart.js";
+import { products } from "../data/products.js";
 
 
 //Access Cart Quantity
 
 let cartQuantity = document.querySelector(".cart-quantity");
 let productArea = document.querySelector(".products-grid");
+
+//Product suffle for random
+for(let i = products.length-1;i>0;i--){
+   const j = Math.floor(Math.random() * (i + 1));
+   [products[i],products[j]] = [products[j],products[i]];
+}
+//add product to homepage
+
 
 let productCard = "";
 products.forEach((product) => {
@@ -20,7 +29,7 @@ products.forEach((product) => {
                 <span class="rating-quantity">${product.rating.count}</span>
             </div>
             <div class="product-price">
-                $${product.priceCents}
+                $${(product.priceCents / 100).toFixed(2)}
             </div>
             <div class="order-quantity">
                 <select class="product-order-select">
@@ -35,7 +44,48 @@ products.forEach((product) => {
             <div class="added-to-cart">Added To Cart</div>
 
             
-            <button class="add-cart-btn">Add to Cart</button>
+            <button class="add-cart-btn" data-product-id = "${product.id}">Add to Cart</button>
         </div>`;
 })
 productArea.innerHTML = productCard;
+
+
+//Adding Cart Interactivity
+
+let btns = document.querySelectorAll(".add-cart-btn");
+let selectOption = document.querySelectorAll(".product-order-select");
+
+//CartUpdate Function
+const cartUpdate = (value) =>{
+       let orderQ = +value;
+       let cartValue= +cartQuantity.innerText;
+       cartQuantity.innerText = cartValue+orderQ;
+}
+
+
+btns.forEach((btn,index)=>{
+   btn.addEventListener("click",()=>{
+
+
+      cartUpdate(selectOption[index].value);
+
+      const productId = btn.dataset.productId;
+      console.log(productId);
+
+      if(cart.length === 0){
+         cart.push(
+         {
+               id:productId,
+               quantity:selectOption[index].vlaue,
+         }
+
+         )
+      }
+      
+
+
+
+
+   })
+
+})
